384a9ebdc52c5f34eaff1df353846040f9f9016d
user5497 <nk3457@naver.com>
2023-11-16 00:15:40 +0900

    메뉴 정보 저장 기능 구현.

--- Sources/menu.py
+++ Sources/menu.py
@@ -1,16 +1,46 @@
 ##### 선호도 기반 음식 추천 프로그램 ####
 
-# 17일까지 : add menu
+# menu editing func
+# 17일까지 : add menu 완성 & delete menu 진행하기. 
+
+
+
+menu = {} # 카테고리와 메뉴 저장. 
+meno = {} # 음식별 메모.
+ratings = {} # 음식별 별점. 
+
+# load data
+lines = open("./menus.csv","r",encoding = "utf8").readlines()
+for line in lines:
+    tokens = line.strip().split(",")
+    for i in range(1,len(tokens)-2,2):
+        ratings[tokens[i]] = tokens[i+1] # 음식별 별점 할당. 
+    menu[tokens[0]] = ratings
+    
+
+# 테스트
+#print("menu",menu)
+#print("ratings",ratings)
 
 
 
 # 선택지 제공(메뉴 추가, 메뉴 삭제, 메뉴 추천) // 완료 시 파일 저장 기능
-print(""" 
+
+while True: 
+    print(""" 
       1. 메뉴 추가 
       2. 메뉴 삭제 
       3. 메뉴 추천 
       """)
-choice = int(input("할 일을 선택해주세요. (1~3)"))
+    choice = int(input("할 일을 선택해주세요. (1~3)"))
 
-# 1~3 이외 예외처리하기 
+    if (choice == 1):
+        print("메뉴를 추가합니다.")
+        print("메뉴", *menu.keys())
+        add_cate = input("메뉴 추가를 원하는 카테고리를 선택해 주세요. ")
+        
+        
 
+        
+        
+  
