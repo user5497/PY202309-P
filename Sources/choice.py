b720032397791cbe8233a65d3d06dbcc63d0b67b
user5497 <nk3457@naver.com>
2023-12-06 22:59:01 +0900

    pandas 도입 진행, 파일 저장 기능 진행

--- Sources/choice.py
+++ Sources/choice.py
@@ -1,4 +1,26 @@
-def del_m(menus,ratings):
+
+# choice == 1 메뉴 추가
+def add_menu(menus,ratings): 
+    while True: # 알맞은 카테고리만 입력받기 
+        add_cate = input("메뉴 추가를 원하는 카테고리를 선택해 주세요. ")
+        if add_cate not in menus.keys():
+            print("입력한 카테고리가 존재하지 않습니다. ") 
+        else:
+            break
+        
+    add_menu = input("추가를 원하는 메뉴를 입력해 주세요. ")
+    add_rat = input("추가한 메뉴에 별점을 남겨 주세요. ")
+        
+    menus[add_cate].append(add_menu) 
+    ratings[add_menu] = add_rat
+    print("메뉴 추가가 완료되었습니다. ")
+
+    #print("menu", menus[add_cate])
+    #print("ratings",ratings)
+
+
+# choice == 2 메뉴 삭제
+def del_menu(menus,ratings):
     while True: # 카테고리 존재하지 않는 경우 삭제. 
         del_cate = input("삭제를 원하는 메뉴가 있는 카테고리를 선택해 주세요. ")
         if del_cate not in menus.keys():
--- Sources/main.py
+++ Sources/main.py
@@ -1,31 +1,18 @@
 ##### 선호도 기반 음식 추천 프로그램 ####
 
-# ~12/1 File Storage Func 완성 // 일정 밀림. 
-# memos 저장 기능도 같이 추가하기. 
 
-## 일정 조금씩 밀림, 일정 나중에 수정하기. 
-# 메뉴 추천 기능 시작, 카테고리 선택 완성 및 메모 저장 기능 진행하기.  
-# 카테고리 선택: 음식을 추천받고자 하는 카테고리 선택. 
-# 메모 저장: 추천된 음식에 코멘트 남기기. 
+import pandas as pd
+# 선택지별 기능
+import choice
+# 파일 관련 기능
+#import file_adit0 as fa 
 
-import file_adit0 as fa # 파일 입출력기능.
-#import file_edit as fa
-import add_menu
-import del_menu
 
-# 메뉴, 선호도 등 정보 가져오기 
-menus = {} # 카테고리와 메뉴 저장. 
-ratings = {} # 음식별 별점. 
-fa.load_file("./menus.csv",menus,ratings)
+# 정보 가져오기
+data= pd.read_csv('menus.csv')
+print("data",data)
 
-#메모 가져오기 
-memos = {} # 음식별 메모. 
-memos = fa.load_file("./memos.csv",menus,ratings)
 
-# 테스트 
-print("menu",menus)
-print("ratings",ratings)
-print("momos",memos)
     
 while True: 
     print(""" 
@@ -38,14 +25,14 @@ while True:
 
     if (choice == 1):
         print("메뉴를 추가합니다.")
-        print("메뉴: ", *menus.keys()) # 카테고리 보여주기
+        print("메뉴: ", data['Categpry']) # 카테고리 보여주기
 
-        add_menu.add_m(menus,ratings)
+        choice.add_menu(menus,ratings)
     
     elif (choice == 2): 
         print("메뉴를 삭제합니다. ")
         print(*menus.keys())
-        del_menu.del_m(menus,ratings)
+        choice.del_menu(menus,ratings)
         
 
     elif (choice == 3 ):
@@ -54,10 +41,22 @@ while True:
     elif (choice == 4): 
         # 종료 및 파일에 반영
         print("종료합니다. ")
-        fa.save_file("./menus.csv",menus,ratings)
+        #fa.save_file("./menus.csv",menus,ratings)
         break
 
     else: 
         print("잘못된 번호를 선택했습니다. 다시 입력해 주세요.")
         continue
             
+
+
+
+# pandas 도입하고 내용 너무 간단해짐. 추가할 내용 생각해보기 
+# 오류처리 까먹지말기. (try - except)
+
+# ~12/1 File Storage Func 완성 // 일정 밀림. 
+# memos 저장 기능도 같이 추가하기. 
+
+# 메뉴 추천 기능 시작, 카테고리 선택 완성 및 메모 저장 기능 -진행-하기. 
+# 카테고리 선택: 음식을 추천받고자 하는 카테고리 선택. 
+# 메모 저장: 추천된 음식에 코멘트 남기기. 
