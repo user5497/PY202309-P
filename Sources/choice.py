34cab3be82eea09934f6192cd231c7b483965115
user5497 <nk3457@naver.com>
2023-12-24 15:48:52 +0900

    Final Report

--- Sources/choice.py
+++ Sources/choice.py
@@ -1,33 +1,39 @@
+import random
 
-# 1번 메뉴 추가 
-def add_menu(menus,ratings,memos): 
-    while True: # 알맞은 카테고리만 입력받기 
+# 메뉴 추가 
+def add_menu(menus,memos): 
+    while True: # 존재하지 않는 카테고리를 입력받는 경우 차단. 
         add_cate = input("메뉴 추가를 원하는 카테고리를 선택해 주세요. ")
-        if add_cate not in menus.keys():
+        if add_cate not in menus.keys(): 
             print("입력한 카테고리가 존재하지 않습니다. ") 
         else:
             break
 
     add_menu = input("추가를 원하는 메뉴를 입력해 주세요. ")
-    add_rat = input("추가한 메뉴에 별점을 남겨 주세요. ")
+
+    while True: # 정수만 입력 
+        try:
+            add_rate = int(input("추가한 메뉴에 별점을 남겨 주세요. "))
+            break
+        except ValueError: 
+            print("숫자를 입력해 주세요. ")
+    
+
     input_memo = input("메모를 남기시겠습니까? (y/n)")
-    memo = " "
+    memo = " " # 메모를 남기지 않는 경우
     if (input_memo == 'y'):
         memo = input("메모를 입력해주세요. ")
 
-    menus[add_cate].add(add_menu) 
-    ratings[add_menu] = add_rat
+    menus[add_cate][add_menu] =  add_rate
     memos[add_menu] = memo
     print("메뉴 추가가 완료되었습니다. ")
-
-    #print("menu", menus[add_cate])
-    #print("ratings",ratings)
-
+    #print("menus",menus)
+    #print("memos",memos)
 
 
-# 2번 메뉴 삭제
-def del_menu(menus,ratings,memos):
-    while True: # 카테고리 존재하지 않는 경우 삭제. 
+# 메뉴 삭제
+def del_menu(menus,memos):
+    while True: # 존재하지 않는 카테고리를 입력받는 경우 차단. 
         del_cate = input("삭제를 원하는 메뉴가 있는 카테고리를 선택해 주세요. ")
         if del_cate not in menus.keys():
             print("선택한 카테고리가 존재하지 않습니다. 다시 선택해 주세요. ")
@@ -44,10 +50,28 @@ def del_menu(menus,ratings,memos):
         else:
             break
 
-    menus[del_cate].remove(del_menu)
-    del ratings[del_menu]
+    del menus[del_cate][del_menu]
     del memos[del_menu]
     print("삭제가 완료되었습니다. ")
 
-    #print("menus",menus[del_cate])
-    #print("ratings",ratings)
+    print("menus",menus[del_cate])
+    print("memos",memos)
+
+
+# 메뉴 추천.
+def RecommendMenu(recommend_menus):
+    total_weight = sum(recommend_menus.values()) # rating 총합
+    rand_val = random.uniform(0,total_weight) # 0~ total weight 사이 무작위 실수 추출. 
+
+    shuffled_rating = list(recommend_menus.items()) 
+    random.shuffle(shuffled_rating) # ratings를 랜덤으로 섞은 shuffled_rating 반환. 
+    shuffled_rating = dict(shuffled_rating)
+    # print("shuffled_rating",shuffled_rating) # 테스트 
+
+    current_weight = 0 # 초기화 
+    for menu,rating in shuffled_rating.items():
+        current_weight += rating # 뒤로 갈수록 점점 확률이 높아짐, 자체 선호도가 높아도 추천 확률 높아짐. 
+        if rand_val <= current_weight: # 누적 선호도가 랜덤 값보다 높아지는 순간 선택. 
+            return menu
+                
+        
--- Sources/main.py
+++ Sources/main.py
@@ -1,40 +1,32 @@
 ##### 선호도 기반 음식 추천 프로그램 ####
+# 전반적 변수 가독성 개선 필요. 
+
 
 import pandas as pd
 import choice # 선택지
-import random
-
 
 # 정보 가져오기
 data= pd.read_csv('menus.csv') 
 
 # 정보 저장.  
-menus = {} # {category1 : {menu1,menu2....}, category2 : {....}}
-end_menu = {}
+menus = {} 
 for index, row in data.iterrows():
     category = row['Category']
     menu = row['Menu']
-
     if category not in menus: # 새로운 카테고리 추가. 
         menus[category] = {}
     menus[category][menu] = row['Ratings']
 
-ratings = {} # Menu : Ratings
-for index, row in data.iterrows():
-    menu = row['Menu']
-    rating = row['Ratings']
-    ratings[menu] = rating
-
-memos = {} # Menu : Memo
+memos = {} 
 for index, row in data.iterrows():
     menu = row['Menu']
     memo = row['Memo'] 
     memos[menu] = memo
 
  # 테스트
-print("menus",menus)
-print("ratings",ratings)
+#print("menus",menus)
 #print("memos",memos)
+
 
 while True: 
     print(""" 
@@ -43,75 +35,78 @@ while True:
       3. 메뉴 추천 
       4. 종료
       """)
-    choice_todo = int(input("할 일을 선택해주세요. (1~4)"))
+      
+    while True: # 정수만 입력 
+        try:
+            choice_todo = int(input("할 일을 선택해주세요. (1~4)"))
+            break
+        except ValueError: 
+            print("숫자를 입력해 주세요. ")
+    
 
-    if (choice_todo == 1):
-        print(*menus.keys()) # 카테고리 보여주기 
-        choice.add_menu(menus,ratings,memos)
+    if (choice_todo == 1): # 메뉴 추가. 
+        print(*menus.keys()) # 가독성. 
+        choice.add_menu(menus,memos)
 
 
     elif (choice_todo == 2): 
         print("메뉴를 삭제합니다. ")
-        choice.del_menu(menus,ratings,memos)
+        print(*menus.keys())
+        choice.del_menu(menus,memos)
         
 
     elif (choice_todo == 3 ):
-        
-        print(*menus.keys())
-        while True:
-            recommend_cate = input("메뉴를 추천받을 카테고리를 선택해 주세요.")
-            if recommend_cate not in menus.keys():
-                print("선택한 카테고리가 존재하지 않습니다. 다시 선택해 주세요. ")
-            else:
+        print(*menus.keys()) 
+        while True: # 존재하지 않는 카테고리를 입력받는 경우 차단. 
+            while True: 
+                recommend_cate = input("메뉴를 추천받을 카테고리를 선택해 주세요.")
+                if recommend_cate not in menus.keys():
+                    print("선택한 카테고리가 존재하지 않습니다. 다시 선택해 주세요. ")
+                else:
+                    break
+
+            # 선택한 카테고리의 정보만 저장하는 recommned_menus 생성.
+            recommend_menus = menus.get(recommend_cate) 
+            recommend_menu = choice.RecommendMenu(recommend_menus)
+            print(f"{recommend_menu}(을/를) 추천합니다.")  
+            print("이 메뉴에 남긴 메모는", memos[recommend_menu],"입니다.")
+
+
+            while True: # 정수만 입력 
+                try:
+                    new_rate = int(input(f"{recommend_menu}의 별점을 남겨 주세요."))
+                    break
+                except ValueError: 
+                    print("숫자를 입력해 주세요. ")
+  
+            input_memo = input("메모를 남기시겠습니까? (y/n)")
+            new_memo = " "
+            if (input_memo == 'y'):
+                new_memo = input("메모를 입력해주세요. ")
+
+            menus[recommend_cate][recommend_menu] = new_rate
+            memos[recommend_menu] = new_memo
+            
+
+            again = input("다시 추천받으시겠습니까? (y/n)").lower()
+            if (again == 'y'):
+                continue
+            else: # 잘못된 입력도 추천 종료로 간주. 
+                print("추천을 종료합니다. ")
                 break
-
-
-        # 카테고리의 끝까지만 추출해 사용할 필요. 
-        # menus category의 카테고리별 마지막 값 찾기 
-        end_menus = {} # 카테고리별 마지막 메뉴들 저장. 
-        for category,menu in menus.items():
-            print(menu)
-            end_menus[category] = list(menu)[-1]
-
-        print("end_menus",end_menus)
-
-
-        recommend_ratings = {} # 정한 카테고리의 end menu까지만 저장하는 새로운 dict 생성. 
-        for menu in ratings.keys():
-            if menu == end_menus[recommend_cate]: 
-                break  
-            recommend_ratings[menu] = ratings[menu]
-        print("recommend_ratings",recommend_ratings)
-
-
-        def WeightedRandom(ratings):
-            total_weight = sum(ratings.values()) # rating 총합
-            rand_val = random.uniform(0,total_weight) # 0~ total weight 사이 무작위 실수 추출. 
-
-            shuffled_rating = list(ratings.items()) 
-            random.shuffle(shuffled_rating) # ratings를 랜덤으로 섞은 shuffled_rating 반환. 
-
-            current_weight = 0 # 초기화 
-            for menu,rating in shuffled_rating.items():
-                current_weight += rating # 뒤로 갈수록 점점 확률이 높아짐, 자체 선호도가 높아도 추천 확률 높아짐. 
-                if rand_val <= current_weight: # 누적 선호도가 랜덤 값보다 높아지는 순간 선택. 
-                    return menu
-                
-        #WeightedRandom(ratings.values())  
 
 
     # 종료 및 파일에 반영
     elif (choice_todo == 4): 
         print("종료합니다. ")
 
-        # menus, ratings, memos를 DataFrame으로 변환
-        menus_df = pd.DataFrame(list(menus.items()), columns=['Category', 'Menu']).explode('Menu').reset_index(drop=True)
-        ratings_df = pd.DataFrame(list(ratings.items()), columns=['Menu', 'Ratings'])
+        # menus, memos를 DataFrame으로 변환
+        menus_df = pd.DataFrame([(cate, menu, rate) for cate, menu_rates in menus.items() for menu, rate in menu_rates.items()]\
+                                , columns=['Category', 'Menu', 'Ratings'])
         memos_df = pd.DataFrame(list(memos.items()), columns=['Menu', 'Memo'])
 
-        # menus, ratings, memos를 병합
-        result_df = pd.merge(menus_df, ratings_df, on='Menu')
-        result_df = pd.merge(result_df, memos_df, on='Menu')
+        # menus, memos를 병합
+        result_df = pd.merge(menus_df, memos_df, on='Menu')
 
         # 결과를 csv 파일로 저장
         result_df.to_csv('menus.csv', index=False)
@@ -120,23 +115,5 @@ while True:
     else: 
         print("잘못된 번호를 선택했습니다. 다시 입력해 주세요.")
         continue
-            
 
-
-
-
-# 완성헤야 하는 것. 
-# Menu Recommendation (대주제)
-# - Save Memo : 추천된 메뉴에 메모 남기기. 
-# - Select category : 메뉴를 추천받을 카테고리 선정하기 
-# - Save Preferences : 추천된 메뉴에 선호도를 남기고 
-# 새로운 음식 추천에 반영될 수 있도록 하기 
-
-
-# 완성한 것. (보완 필요)
-# - Add Menu
-# - Delete Menu
-# - File Storage Function
-
-
-# try - except 잘 쓰기 
+    
